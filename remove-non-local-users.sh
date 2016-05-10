bc5317e33c54defe90e51610355d9f35a6476b64
John Mahlman <john.mahlman@gmail.com>
2016-05-10 14:30:15 -0400

    New remove users script
    Our original script to remove non-local/AD users was throwing errors so
    I decided to create a new one.
    
    This one seems to work better and also gives us the ability to remove
    accounts based on other criteria (modified date, User group, etc)

---
+++ remove-non-local-users.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+########################################################################
+# Created by John Mahlman, University of the Arts Philadelphia (jmahlman@uarts.edu)
+# Last Updated May 10, 2016
+#
+# Name: remove-non-local-users
+# Purpose: Removes all non-local accounts on machines to help stop HDs from filling up
+# Will spare the 'macadmin' and 'Shared' home directories.
+########################################################################
+
+users=`find /Users -type d -maxdepth 1 | cut -d"/" -f3` 
+# you can edit this to remove only accounts that haven't logged in for x days: add '-mtime +<# of days>' after maxdepth
+
+for i in $users; do
+    if [[ $i = "macadmin" ]] || [[ $i = "Shared" ]]; then continue
+    else 
+        jamf deleteAccount -username $i -deleteHomeDirectory
+        rm -Rf /Users/$i
+    fi
+done
