750652475c4ac4c7d7c9bdd1df335a8c222f4e06
John Mahlman <john.mahlman@gmail.com>
2016-05-18 13:55:47 -0400

    New script
    I use this to add icons for Office 2016 after installation.  This can
    be customized for whatever other icons are needed.

---
+++ add-office-icons.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+#
+#
+# Created by John Mahlman, University of the Arts Philadelphia (jmahlman@uarts.edu)
+# Last Updated: 5/18/16
+#
+# Name: add-office-icons
+#
+# Purpose: Adds office icons to dock using dockutil
+# Also installs dockutil if it's not found
+#
+
+jamfbinary=$(/usr/bin/which jamf)
+
+# Make sure we have dockutil, install it if we don't
+if [ ! -f "/usr/local/bin/dockutil" ]; then
+	echo "Installing DockUtil from JSS"
+	"$jamfbinary" policy -event dockutil
+	if [ ! -f "/usr/local/bin/dockutil" ]; then
+		echo "Unable to install DockUtil, aborting!"
+		exit 1
+	fi
+fi
+du="/usr/local/bin/dockutil"
+
+# Get the current user
+if [ ! -n "$3" ]; then
+	user=$3
+else
+	user=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+fi
+
+##########
+# Script #
+##########
+
+# Add dock icons only if the app is installed
+if [ -e "/Applications/Microsoft Word.app" ]; then
+	echo "Adding Office 2016 apps to dock"
+	sleep 2
+	$du --add "/Applications/Microsoft Word.app" --replacing "Microsoft Word" --no-restart /Users/$user
+	$du --add "/Applications/Microsoft Excel.app" --replacing "Microsoft Excel" --no-restart /Users/$user	
+	$du --add "/Applications/Microsoft Outlook.app" --replacing "Microsoft Outlook" --no-restart /Users/$user	
+	$du --add "/Applications/Microsoft Powerpoint.app" --replacing "Microsoft Powerpoint" --no-restart /Users/$user
+	# Restart the dock when done
+	sleep 5
+	killall Dock
+	exit 0
+else
+	echo "Office 2016 not installed!  Exiting."
+	exit 1
+fi
