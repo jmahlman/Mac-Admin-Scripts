7d929fbe64e005a69a6539afb9c420f7c773b100
John Mahlman <john.mahlman@gmail.com>
2016-05-18 13:54:35 -0400

    Fixed a bunch of things!
    Changed how we find current logged in user to the “Apple Preferred”
    method (this accounts for fast user switching too).
    Changed everything to “elif” statements  to make it run better.
    Removed an extra space on line 62.
    
    Lots of other things.

--- dockMaster.sh
+++ dockMaster.sh
@@ -4,13 +4,14 @@
 # Original created By: Colin Bohn, Stanwood-Camano School District (cbohn@scsd.ac)
 # 
 # Customized by John Mahlman, University of the Arts Philadelphia
-# Last Updated May 16, 2016
+# Last Updated May 1, 2016
 #
 # Name: DockMaster
 # Purpose: Set the contents of the dock on login based on computer type (cohort) 
 # and what applications are available on the local machine.
 ########################################################################
 
+sleep 5 # we need to wait for the dock to actually start
 
 #######################################
 #### Where's our JAMF binary?
@@ -36,19 +37,18 @@ du="/usr/local/bin/dockutil"
 if [ ! -n "$3" ]; then
 	user=$3
 else
-	user=$(/usr/bin/who | /usr/bin/awk '/console/{ print $1 }')
+	user=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
 fi
-sleep 5 # we need to wait for the dock to actually start
 echo "Running DockMaster on $user"
 
 #######################################
 ##### Let's find that cohort...
 #######################################
 if [ -f /Library/JAMF\ DM/Cohort/*.txt ]; then
-	cohort=`cat /Library/JAMF\ DM/Cohort/*.txt`
+	cohort=$(cat /Library/JAMF\ DM/Cohort/*.txt)
 	echo "Cohort set to $cohort"
 else
-	echo "No cohort available! Using settings for NO COHORT."
+	echo "No cohort available! Adding default icons cohort"
 	cohort="NONE"
 fi
 
@@ -59,7 +59,7 @@ officeIcons ()
 {
 	wordversion=$(/usr/bin/mdls -name kMDItemVersion "/Applications/Microsoft Word.app/")
 	# Checking for Office 2016
-	if [[ $wordversion == *" 15."* ]]; then
+	if [[ $wordversion == *"15."* ]]; then
 		echo "Adding Office 2016 apps"
 		$du --add "/Applications/Microsoft Word.app" --no-restart /Users/$user
 		$du --add "/Applications/Microsoft Excel.app" --no-restart /Users/$user
@@ -74,7 +74,7 @@ officeIcons ()
 			$du --add "/Applications/Microsoft Office 2011/Microsoft Outlook.app" --no-restart /Users/$user
 			$du --add "/Applications/Microsoft Office 2011/Microsoft Powerpoint.app" --no-restart /Users/$user
 		fi
-	fi # End office additions here
+	fi
 }
 
 #######################################
@@ -82,45 +82,42 @@ officeIcons ()
 #######################################
 echo "Removing all items from the dock"
 $du --remove all --no-restart /Users/$user
-sleep 5 # we need to give this time to work or we'll get errors with "replacing" items instead of adding them.
+sleep 2 # we need to give this time to work or we'll get errors with "replacing" items instead of adding them.
 
 #######################################
-#### Everyone likes a Downloads folder
-#######################################
-echo "Adding the Downloads folder"
-$du --add "~/Downloads" --view fan --display stack --sort dateadded --no-restart /Users/$user
-
-#######################################
-#### Add universal apps
-#### This runs for all cohorts
+#### Items for all/no cohorts
 #######################################
 echo "Adding browsers"
-$du --add "/Applications/Safari.app" --no-restart /Users/$user
 
-if [ -d "/Applications/Google Chrome.app/" ]; then
+$du --add "/Applications/Safari.app" --no-restart /Users/$user
+if [ -e "/Applications/Google Chrome.app/" ]; then
 	$du --add "/Applications/Google Chrome.app" --no-restart /Users/$user
 fi
-
-if [ -d /Applications/Firefox* ]; then
-	firefox=`find /Applications -type d -maxdepth 1 -name Firefox*`
+# We have two different firefox types, lets figure out which one is installed
+if [ -e /Applications/Firefox* ]; then
+	firefox=$(find /Applications -type d -maxdepth 1 -name Firefox*)
 	$du --add "$firefox" --no-restart /Users/$user
 fi
+# Add Office icons to all cohorts
+officeIcons
+# Every user gets a downloads folder too
+echo "Adding the Downloads folder"
+$du --add "~/Downloads" --view fan --display stack --sort dateadded --no-restart /Users/$user	
 
 #######################################
-#### Add dock items for FACSTAFF only
+#### Add dock items for FACSTAFF cohort
 #######################################
 if [ $cohort == "FACSTAFF" ]; then
 	echo "Adding apps for FACSTAFF"
-	officeIcons
 	$du --add "/Applications/Calendar.app" --no-restart /Users/$user
 	$du --add "/Applications/Preview.app" --no-restart /Users/$user
 	$du --add "/Applications/iTunes.app" --no-restart /Users/$user
 	$du --add "/Applications/Photo Booth.app" --no-restart /Users/$user
 	$du --add "/Applications/Time Machine.app" --no-restart /Users/$user
-	if [ -d "/Library/KeyAccess/KeyCheckout.app/" ]; then
+	if [ -e "/Library/KeyAccess/KeyCheckout.app/" ]; then
 		$du --add "/Library/KeyAccess/KeyCheckout.app" --no-restart /Users/$user
 	fi
-	if [ -d "/Applications/Network Connect.app/" ]; then
+	if [ -e "/Applications/Network Connect.app/" ]; then
 		$du --add "/Applications/Network Connect.app" --no-restart /Users/$user
 	fi
 	$du --add "/Applications/Self Service.app" --no-restart /Users/$user
@@ -129,14 +126,12 @@ if [ $cohort == "FACSTAFF" ]; then
 	# This should be the end of the applications in the dock, anything after should be a folder
 	$du --add "/Applications" --view grid --display folder --sort name --no-restart /Users/$user
 	$du --add "~/Documents" --view fan --display stack --sort dateadded --no-restart /Users/$user
-fi
 
 #######################################
-#### Add dock items for OFFICE only
+#### Add dock items for OFFICE cohort
 #######################################
-if [ $cohort == "OFFICE" ]; then
-	echo "Adding apps for OFFICE only"
-	officeIcons
+elif [ $cohort == "OFFICE" ]; then
+	echo "Adding apps for OFFICE cohort"
 	$du --add "/Applications/Launchpad.app" --position beginning --no-restart /Users/$user
 	$du --add "/Applications/Contacts.app" --no-restart /Users/$user
 	$du --add "/Applications/Calendar.app" --no-restart /Users/$user
@@ -146,30 +141,30 @@ if [ $cohort == "OFFICE" ]; then
 	$du --add "/Applications/System Preferences.app" --position end --no-restart /Users/$user
 	# This should be the end of the applications in the dock, anything after should be a folder
 	$du --add "/Applications" --view grid --display folder --sort name --no-restart /Users/$user
-fi
 
 #######################################
 #### Add dock items for PUBLIC cohorts
 #### This one has a lot of different cohorts,
 #### You can break it down as much as you want
 #######################################
-if [ $cohort == "PUBLIC" ]; then
-	echo "Adding apps for PUBLIC only"
-fi
+elif [ $cohort == "PUBLIC" ]; then
+	echo "Adding apps for PUBLIC cohort"
 
 #######################################
-#### Add dock items for CHECKOUT only
+#### Add dock items for CHECKOUT cohort
 #######################################
-if [ $cohort == "CHECKOUT" ]; then
-	echo "Adding apps for CHECKOUT only"
-fi
+elif [ $cohort == "CHECKOUT" ]; then
+	echo "Adding apps for CHECKOUT cohort"
 
 #######################################
-#### Add dock items for KIOSK only
+#### Add dock items for KIOSK cohort
 #######################################
-if [ $cohort == "KIOSK" ]; then
-	echo "Adding apps for KIOSK only"
-fi
+elif [ $cohort == "KIOSK" ]; then
+	echo "Adding apps for KIOSK cohort"
+
+fi #end of cohorts
 
 # Restart the dock after everything is done
+sleep 5
 killall Dock
+exit 0
